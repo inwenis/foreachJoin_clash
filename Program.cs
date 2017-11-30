fd870913c1f472c2f00bcea539e3d6f539a6a864
Filip Kucharczyk <fku-ext@danskecommodities.com>
2017-11-30 13:05:44 +0100

    remove redundant namespace

--- Program.cs
+++ Program.cs
@@ -2,123 +2,120 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace join_test
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var nominatedUserIds = new List<int>()
         {
-            var nominatedUserIds = new List<int>()
-            {
-                1, 2, 3, 4, 10, 101
-            };
-            var creditCards = new List<CreditCard>()
-            {
-                new CreditCard(1, 1, 400),
-                new CreditCard(2, 2, 401),
-                new CreditCard(3, 3, 402),
-                new CreditCard(4, 4, 403),
-                new CreditCard(5, 5, 404),
-                new CreditCard(6, 6, 405),
-                new CreditCard(7, 7, 406),
-                new CreditCard(8, 8, 407),
-            };
-            var orders = new List<Odrer>()
-            {
-                new Odrer(400),
-                new Odrer(401),
-                new Odrer(402),
-                new Odrer(403),
-                new Odrer(410),
-            };
+            1, 2, 3, 4, 10, 101
+        };
+        var creditCards = new List<CreditCard>()
+        {
+            new CreditCard(1, 1, 400),
+            new CreditCard(2, 2, 401),
+            new CreditCard(3, 3, 402),
+            new CreditCard(4, 4, 403),
+            new CreditCard(5, 5, 404),
+            new CreditCard(6, 6, 405),
+            new CreditCard(7, 7, 406),
+            new CreditCard(8, 8, 407),
+        };
+        var orders = new List<Odrer>()
+        {
+            new Odrer(400),
+            new Odrer(401),
+            new Odrer(402),
+            new Odrer(403),
+            new Odrer(410),
+        };
 
 
 
-            Console.WriteLine("A) linq join");
+        Console.WriteLine("A) linq join");
 
-            var specialOrders = nominatedUserIds
-                .Join(creditCards,
-                    userId => userId,
-                    creditCard => creditCard.UserId,
-                    (_, creditCard) => creditCard)
-                .Join(orders,
-                    creditCard => creditCard.OrderId,
-                    order => order.Id,
-                    (_, order) => order);
+        var specialOrders = nominatedUserIds
+            .Join(creditCards,
+                userId => userId,
+                creditCard => creditCard.UserId,
+                (_, creditCard) => creditCard)
+            .Join(orders,
+                creditCard => creditCard.OrderId,
+                order => order.Id,
+                (_, order) => order);
 
-            DoSomethingWithOrders(specialOrders);
-
-
-
-            Console.WriteLine("B) the same in SQL syntax");
-
-            var specialOrders2 =
-                from userId in nominatedUserIds
-                join creditCard in creditCards on userId equals creditCard.UserId
-                join order in orders on creditCard.OrderId equals order.Id
-                select order;
-
-            DoSomethingWithOrders(specialOrders2);
+        DoSomethingWithOrders(specialOrders);
 
 
 
-            Console.WriteLine("C) the same starting from orders");
+        Console.WriteLine("B) the same in SQL syntax");
 
-            var specialOrders3 =
-                from order in orders
-                join creditCard in creditCards on order.Id equals creditCard.OrderId
-                join userId in nominatedUserIds on creditCard.UserId equals userId
-                select order;
+        var specialOrders2 =
+            from userId in nominatedUserIds
+            join creditCard in creditCards on userId equals creditCard.UserId
+            join order in orders on creditCard.OrderId equals order.Id
+            select order;
 
-            DoSomethingWithOrders(specialOrders3);
+        DoSomethingWithOrders(specialOrders2);
 
 
 
-            Console.WriteLine("D) foreach");
+        Console.WriteLine("C) the same starting from orders");
 
-            foreach (var userId in nominatedUserIds)
-            {
-                var specialOders = orders
-                    .Where(order =>
-                        creditCards.Any(creditCard => creditCard.OrderId == order.Id && creditCard.UserId == userId));
+        var specialOrders3 =
+            from order in orders
+            join creditCard in creditCards on order.Id equals creditCard.OrderId
+            join userId in nominatedUserIds on creditCard.UserId equals userId
+            select order;
 
-                DoSomethingWithOrders(specialOders);
-            }
+        DoSomethingWithOrders(specialOrders3);
 
-            Console.WriteLine("Press enter to exit...");
-            Console.ReadKey();
+
+
+        Console.WriteLine("D) foreach");
+
+        foreach (var userId in nominatedUserIds)
+        {
+            var specialOders = orders
+                .Where(order =>
+                    creditCards.Any(creditCard => creditCard.OrderId == order.Id && creditCard.UserId == userId));
+
+            DoSomethingWithOrders(specialOders);
         }
 
-        private static void DoSomethingWithOrders(IEnumerable<Odrer> specialOders)
-        {
-            foreach (var specialOder in specialOders)
-            {
-                Console.WriteLine(specialOder.Id);
-            }
-        }
+        Console.WriteLine("Press enter to exit...");
+        Console.ReadKey();
     }
 
-    class CreditCard
+    private static void DoSomethingWithOrders(IEnumerable<Odrer> specialOders)
     {
-        public int Id;
-        public int UserId;
-        public int OrderId;
-
-        public CreditCard(int id, int userId, int orderId)
+        foreach (var specialOder in specialOders)
         {
-            Id = id;
-            UserId = userId;
-            OrderId = orderId;
+            Console.WriteLine(specialOder.Id);
         }
     }
+}
 
-    class Odrer
+class CreditCard
+{
+    public int Id;
+    public int UserId;
+    public int OrderId;
+
+    public CreditCard(int id, int userId, int orderId)
     {
-        public int Id;
+        Id = id;
+        UserId = userId;
+        OrderId = orderId;
+    }
+}
 
-        public Odrer(int id)
-        {
-            Id = id;
-        }
+class Odrer
+{
+    public int Id;
+
+    public Odrer(int id)
+    {
+        Id = id;
     }
 }
